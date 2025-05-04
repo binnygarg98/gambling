a752236d66bb134e2e2c8bcdd589076d5b2c58ce
binni garg <binnygarg7@gmail.com>
2025-05-05 00:17:33 +0530

    changes

--- controllers/couponController.js
+++ controllers/couponController.js
@@ -134,8 +134,9 @@ LIMIT 1;
 
         let minPrize = couponData.min_prize_amount;
         let maxPrize = couponData.max_prize_amount;
-        let prizeAmount = Math.floor(Math.random() * (maxPrize - minPrize + 1)) + minPrize;
-    
+        // let prizeAmount = Math.floor(Math.random() * (maxPrize - minPrize + 1)) + minPrize;
+        let prizeAmount = couponData.max_prize_amount;
+       
         let userWallet = await UserWallet.findOne({ where: { user_id: userId }, transaction: t });
         if (!userWallet) {
             await t.rollback();
